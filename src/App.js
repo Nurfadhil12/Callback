d7a713c2e0a28d0a89e213bf4c14587c8b5f2269
Nurfadhil12 <nuurrff@gmail.com>
2021-10-22 15:15:28 +0700

    Ashyncronus

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import React from "react";
+import MiniTable  from './MiniTable';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Callback from './Callback';
+
+
+
+function App() {
+    return ( 
+        <div className = "App">
+            {/* heloo
+            <MiniTable/> */}
+            <Callback/>
+         </div>
+        
+    );
+}
+
+export default App;
