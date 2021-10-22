48ebc10c2936c82748488844af76d158b300322c
Nurfadhil12 <nuurrff@gmail.com>
2021-10-22 15:26:52 +0700

    update text

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
         <div className = "App">
             {/* heloo
             <MiniTable/> */}
+            <h1>List Of Users</h1>
             <Callback/>
          </div>
         
